f0e0b7cff966d242f7152e9d2cc922bf4968b1ea
za-h-ra <zahra.khan1@icloud.com>
2025-07-14 14:46:46 -0400

    [WIP] JS + CSS

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel');
 
-// const removeActiveClasses = () => {
-//   panels.forEach((panel) => {
-//     panel.classList.remove('active');
-//   });
-// };
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
 
-// panels.forEach((panel) => {
-//   panel.addEventListener('click', () => {
-//     removeActiveClasses();
-//     panel.classList.add('active');
-//   });
-// });
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
